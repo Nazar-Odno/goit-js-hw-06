af7f047c0ab4322db43b7af726a9a9ea4e1a61d5
Nazar-Odno <114081040+Nazar-Odno@users.noreply.github.com>
2023-07-29 20:56:41 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulIngredients = document.getElementById("ingredients");
+
+// Створення елементів <li> для кожного елемента масиву ingredients
+const listItems = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  return li;
+});
+
+// Додавання всіх елементів <li> до списку ul#ingredients за одну операцію
+ulIngredients.append(...listItems);
